01e3a3a7d63bf352edca959247e671f55ec4393a
phong-dssi <phongam@dssi.vn>
2025-05-29 18:11:41 +0700

    change index file content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,6 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
+          <h1>Learn &nbsp;	</h1>
           <Image
             className={styles.logo}
             src="/next.svg"
