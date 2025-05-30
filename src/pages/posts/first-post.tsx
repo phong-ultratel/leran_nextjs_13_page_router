e98b6a076e9f103f997b5e31280ef7d4163c8e41
phong-dssi <phongam@dssi.vn>
2025-05-30 15:57:10 +0700

    static resource and head component

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import Layout from '../../../components/layout'
-
+import Head from "next/head";
 export default function FirstPost() {
     return (
         <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href={'/'}>Back to home</Link>
